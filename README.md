2b439fa5988dea511548c881846dd3f91358fec7
Jeff Sheffield <jeff.sheffield@gmail.com>
2016-12-26 11:08:22 -0600

    work in progress

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Take the alpine python docker container, marry it with a npm env ( for Angular )
 and marry that with the Google Application Engine install and wala. You get a 
 isolated container with all you need to deploy.
 
+
+Forked from this:
+https://hub.docker.com/_/python/
+https://github.com/jsheffie/python/blob/master/2.7/alpine/Dockerfile
+
+Compare to this:
+https://github.com/GoogleCloudPlatform/python-runtime
+
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#GAE_DEPLOY="GAE_Python_SDK"
-GAE_DEPLOY="GAE_Original_SDK"
+#GAE_DEPLOY="GAE-Python-SDK"
+GAE_DEPLOY="gae-orig-sdk"
 docker build -t jsheffie/$GAE_DEPLOY .
 docker push jsheffie/$GAE_DEPLOY
 
-ssh  learn << EOF
-docker pull jsheffie/$GAE_DEPLOY:latest
-docker stop web || true
-docker rm web || true
-docker rmi jsheffie/$GAE_DEPLOY:current || true
-docker tag jsheffie/$GAE_DEPLOY:latest jsheffie/$GAE_DEPLOY:current
-docker run -d --restart always --name web -p 3000:3000 jsheffie/$GAE_DEPLOY:current
-EOF
+# ssh  learn << EOF
+# docker pull jsheffie/$GAE_DEPLOY:latest
+# docker stop web || true
+# docker rm web || true
+# docker rmi jsheffie/$GAE_DEPLOY:current || true
+# docker tag jsheffie/$GAE_DEPLOY:latest jsheffie/$GAE_DEPLOY:current
+# docker run -d --restart always --name web -p 3000:3000 jsheffie/$GAE_DEPLOY:current
+# EOF
