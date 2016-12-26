945ba4f6314d3b80685232a3015816271f4b6d7e
Jeff Sheffield <jeff.sheffield@gmail.com>
2016-12-26 10:48:44 -0600

    WIP: working twards an image

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gae_dev
+
+Take the alpine python docker container, marry it with a npm env ( for Angular )
+and marry that with the Google Application Engine install and wala. You get a 
+isolated container with all you need to deploy.
+
---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#GAE_DEPLOY="GAE_Python_SDK"
+GAE_DEPLOY="GAE_Original_SDK"
+docker build -t jsheffie/$GAE_DEPLOY .
+docker push jsheffie/$GAE_DEPLOY
+
+ssh  learn << EOF
+docker pull jsheffie/$GAE_DEPLOY:latest
+docker stop web || true
+docker rm web || true
+docker rmi jsheffie/$GAE_DEPLOY:current || true
+docker tag jsheffie/$GAE_DEPLOY:latest jsheffie/$GAE_DEPLOY:current
+docker run -d --restart always --name web -p 3000:3000 jsheffie/$GAE_DEPLOY:current
+EOF
